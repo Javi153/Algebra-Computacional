531e2d715f9d7c11d09084756a1bdd4035118afb
Javi153 <javijimenez153@gmail.com>
2023-09-21 19:25:21 +0200

    Cambios menores

--- Ejercicios/Tema 2/ej9.py
+++ Ejercicios/Tema 2/ej9.py
@@ -1,19 +1,12 @@
 #Javier Jimenez Arenas
-import random
-def min_suma_casilla(n, m, a):
+def min_suma_casillas(n, m, a): #Como solo podemos ir hacia abajo o hacia la derecha, la forma de llegar a una casilla
+                                #con coste minimo sera precisamente el minimo entre llegar a la casilla de su izquierda y llegar a su casilla superior
     for i in range(n):
         for j in range(m):
-            if i != 0 and j != 0:
+            if i != 0 and j != 0: #Si ambas casillas estan disponibles, buscamos el minimo entre estas y lo guardamos sobre la propia matriz
                 a[i][j] = min(a[i - 1][j] + a[i][j], a[i][j - 1] + a[i][j])
-            elif i != 0:
+            elif i != 0: #Si no existe casilla izquierda la unica manera de llegar es ir siempre hacia abajo, asi que el coste sera el de su casilla superior mas el de la propia casilla objetivo
                 a[i][j] = a[i - 1][j] + a[i][j]
-            elif j != 0:
+            elif j != 0: #Un caso analogo al anterior pero esta vez en el caso en el que no existe casilla superior
                 a[i][j] = a[i][j-1] + a[i][j]
-    return a[n-1][m-1]
-
-mat = [[0 for i in range(1000)] for j in range(1000)]
-for i in range(1000):
-    for j in range(1000):
-        mat[i][j] = random.randint(0, 1348451384315384351834)
-
-print(min_suma_casilla(1000, 1000, mat))
+    return a[n-1][m-1] #Devolvemos el coste minimo en la casilla de mas abajo a la derecha
